c5db32ed7413a8b767d97c81830d63e50f43b41e
Mark Wasfy <76422492+MarkWasfy00@users.noreply.github.com>
2022-08-08 18:56:07 +0200

    fix email config

--- resources/views/emails/demoMail.blade.php
+++ resources/views/emails/demoMail.blade.php
@@ -5,6 +5,7 @@
 </head>
 <body>
     <h1>Name : {{ $mailData['name'] }}</h1>
+    <h2>Email From {{ $mailData['email'] }}</h2>
     <h3>{{ $mailData['message'] }}</h3>
     <p>Thank you</p>
 </body>
